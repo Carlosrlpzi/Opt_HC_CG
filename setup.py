762a313ed16a7096ad3807c54a2d5968bac33bbf
Carlosrlpzi <53799984+Carlosrlpzi@users.noreply.github.com>
2021-04-29 21:35:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Carlos',                   # Type in your name
   author_email = 'carlosrlpzi@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Carlosrlpzi/Opt_HC_CG',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Carlosrlpzi/Opt_HC_CG/archive/refs/tags/0.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Carlosrlpzi/Opt_HC_CG/archive/refs/tags/0.3.tar.gz',    # I explain this later on
   keywords = ['Hill Climbing', 'TSP'],  # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
