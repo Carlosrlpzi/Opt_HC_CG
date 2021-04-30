6d3eb0c96ca9352a08f79ef3bd2d9bbc0462f466
Carlosrlpzi <53799984+Carlosrlpzi@users.noreply.github.com>
2021-04-29 21:20:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'Opt_HC_CG',         # How you named your package folder (MyLib)
   packages = ['Opt_HC_CG'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Use hill climbing to solve tsp',   # Give a short description about your library
   author = 'Carlos',                   # Type in your name
